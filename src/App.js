a9a6594e0ad401da01f2f028e6465f0cb6a4055c
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-02-02 21:08:51 -0500

    demonstrating styling tactic, renaming stuff to feedbak

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+const stl = {
+    h1: {
+        "textAlign": "center"
+    }
+}
+
 const App = () => ( 
     <div>
-        <h1>Hello React</h1>
+        <h1 style={stl.h1}>Feedbak</h1>
     </div>
 );
 
