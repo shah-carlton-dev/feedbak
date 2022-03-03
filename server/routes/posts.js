05792c95fd898845b13fbf554bd176438e82c923
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-03-03 13:30:13 -0500

    post routes are all good 2 go

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -11,11 +11,10 @@ const postsRouter = express.Router();
  */
 postsRouter.route("/all/:id").get(function (req, res) {
     let db_connect = dbo.getDb("feedbak01");
-    const businessId = ObjectId(req.params.id)
     db_connect
         .collection("review")
         .find({
-            "business": businessId
+            "business": ObjectId(req.params.id)
         })
         .toArray(function (err, result) {
             if (err) throw err;
@@ -28,27 +27,32 @@ postsRouter.route("/all/:id").get(function (req, res) {
  */
 postsRouter.route("/:id").get(function (req, res) {
     let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
     db_connect
         .collection("review")
-        .findOne(myquery, function (err, result) {
-            if (err) throw err;
-            res.json(result);
-        });
+        .findOne(
+            { _id: ObjectId(req.params.id) },
+            function (err, result) {
+                if (err) throw err;
+                res.json(result);
+            });
 });
 
 /**
- * post a new review
+ * post a new review, add review to user and company profiles
+ * expects: 
+ * {
+ * title, authorId, post, businessId
+ * }
  */
-postsRouter.route("/new").post(function (req, response) {
+postsRouter.route("/new").post(async function (req, response) {
     let db_connect = dbo.getDb("feedbak01");
     let review = {
         title: req.body.title,
-        author: ObjectId(req.body.author),
+        author: ObjectId(req.body.authorId),
         post: req.body.post,
-        score: req.body.score,
+        score: 0,
         business: ObjectId(req.body.businessId),
-        featured: req.body.featured,
+        featured: false,
         date: new Date()
     }
     db_connect
@@ -56,7 +60,19 @@ postsRouter.route("/new").post(function (req, response) {
         .insertOne(review, function (err, res) {
             if (err) throw err;
             response.json(res);
-        });
+            db_connect
+                .collection("user")
+                .updateOne(
+                    { _id: ObjectId(req.body.authorId) },
+                    { $push: { reviews: ObjectId(res.insertedId) } }
+                )
+            db_connect
+                .collection("business")
+                .updateOne(
+                    { _id: ObjectId(req.body.businessId) },
+                    { $push: { reviews: ObjectId(res.insertedId) } }
+                )
+        })
 });
 
 /**
@@ -65,31 +81,49 @@ postsRouter.route("/new").post(function (req, response) {
  */
 postsRouter.route("/update/:id").put(function (req, response) {
     let db_connect = dbo.getDb();
-    let review = { _id: ObjectId(req.params.id) };
-    console.log(review)
-    let newReview = {
-        $set: {
-            score: req.body.score,
-        },
-    };
     db_connect
         .collection("review")
-        .updateOne(review, newReview, function (err, res) {
-            if (err) throw err;
-            console.log("1 document updated");
-            response.json(res);
-        });
+        .updateOne(
+            { _id: ObjectId(req.params.id) },
+            { $set: { score: req.body.score } },
+            function (err, res) {
+                if (err) throw err;
+                console.log("1 document updated");
+                response.json(res);
+            });
 });
 
 // delete post by id
 postsRouter.route("/:id").delete((req, response) => {
     let db_connect = dbo.getDb();
-    let myquery = { _id: ObjectId(req.params.id) };
-    db_connect.collection("review").deleteOne(myquery, function (err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        response.json(obj);
-    });
+    db_connect
+        .collection("review")
+        .findOne(
+            { _id: ObjectId(req.params.id) },
+            function (err, res) {
+                if (err) throw err;
+                db_connect
+                    .collection("user")
+                    .updateOne(
+                        { _id: ObjectId(result.author) },
+                        { $pull: { reviews: ObjectId(req.params.id) } }
+                    )
+                db_connect
+                    .collection("business")
+                    .updateOne(
+                        { _id: ObjectId(result.business) },
+                        { $pull: { reviews: ObjectId(req.params.id) } }
+                    )
+                response.json({
+                    deleted: true,
+                    document: res
+                })
+            });
+    db_connect
+        .collection("review")
+        .deleteOne(
+            { _id: ObjectId(req.params.id) }
+        );
 });
 
 module.exports = postsRouter;
