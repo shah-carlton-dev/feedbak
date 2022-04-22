fa2cfe453bae5496e6fb8beb7af5b75a041dd9e9
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-04-21 21:19:46 -0400

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import { Row, Col, Container, Button, Card} from 'react-bootstrap';
+import { Row, Col, Container, Button, Card, ToggleButton } from 'react-bootstrap';
 import Post from './Post';
 import Axios from 'axios';
 import { API_URL } from '../utils/constants';
 import "../css/NavbarContainer.scss"
 import WriteReviewModal from './WriteReviewModal'
 import UserContext from "../utils/UserContext.js";
-import {Link} from 'react-router-hash-link';
+import { Link } from 'react-router-hash-link';
 const Business = (props) => {
 	const [posts, setPosts] = useState([]);
 	const [businessData, setBusinessData] = useState([]);
 	const [showReviewModal, setShowReviewModal] = useState(false)
+	const [filter, setFilter] = useState('newest')
 
 	const { id } = useParams();
 	const { userData, setUserData } = useContext(UserContext);
@@ -27,11 +28,11 @@ const Business = (props) => {
 	const getBusinessData = async () => {
 		const url = API_URL + '/businesses/' + id;
 		try {
-		  await Axios.get(url).then(res => {setBusinessData(res.data);});
+			await Axios.get(url).then(res => { setBusinessData(res.data); });
 		} catch (err) {
-		  console.log("Error retrieving business list")
+			console.log("Error retrieving business list")
 		}
-	  }
+	}
 
 	useEffect(() => {
 		getPostsData();
@@ -39,21 +40,70 @@ const Business = (props) => {
 		console.log(businessData);
 	}, []);
 
+	const buttons = [
+		{
+			title: 'hot',
+			onClick: () => setFilter('hot')
+		}, {
+			title: 'oldest',
+			onClick: () => setFilter('oldest')
+		}, {
+			title: 'newest',
+			onClick: () => setFilter('newest')
+		}
+	];
+
+	const sortPosts = () => {
+		let temp = [...posts]
+		if (filter === 'hot') {
+			return temp.sort(function (a, b) {
+				return b.score - a.score;
+			});
+		} else if (filter === 'oldest') {
+			return temp.sort(function (a, b) {
+				return a;
+			});
+		}
+	}
+
 	return (
 		<div className="">
 			<Container  className="">
 				<Row className="bizHeader">
 					<h1 className="text-center">{businessData.name}</h1>
-					
+
 				</Row>
 
 				<Row>
 					<Col md={9} id="scroll-meeee" className="businessLeftCol">
 						<Row>
+<<<<<<< Updated upstream
 							{posts.map((info, idx) => (
 							<Col key={idx} className="py-3-custom px-5-custom">
 								<Post key={idx} postInfo={info} admin={userData.user.admin} user={userData.user._id} />
+=======
+							<Col className="py-3-custom px-5">
+								{buttons.map((button, idx) => (
+									<ToggleButton
+										key={idx}
+										type="radio"
+										variant="secondary"
+										name="radio"
+										value={button.title}
+										checked={button.title == filter}
+										onClick={(e) => button.onClick()}
+									>
+										{button.title}
+									</ToggleButton>
+								))}
+>>>>>>> Stashed changes
 							</Col>
+						</Row>
+						<Row>
+							{posts.map((info, idx) => (
+								<Col key={idx} className="py-3-custom px-5">
+									<Post key={idx} postInfo={info} admin={userData.user.admin} user={userData.user._id} />
+								</Col>
 							))}
 						</Row>
 					</Col>
@@ -62,49 +112,49 @@ const Business = (props) => {
 							<Button className="purple-button" onClick={() => setShowReviewModal(!showReviewModal)}> New Feedbak </Button>
 							<Col xl={12}>
 								<div className="businessSide">
-								<Card>
-									<Card.Body>
-										<Card.Title>{businessData.name}</Card.Title>
-										<Card.Subtitle className="mb-2 text-muted">{businessData.dateJoined}</Card.Subtitle>
-										<Card.Text>
-											{businessData.about}
-										</Card.Text>
-										<Card.Link href={businessData.website} target="_blank">website</Card.Link>
-									</Card.Body>
-								</Card>
+									<Card>
+										<Card.Body>
+											<Card.Title>{businessData.name}</Card.Title>
+											<Card.Subtitle className="mb-2 text-muted">{businessData.dateJoined}</Card.Subtitle>
+											<Card.Text>
+												{businessData.about}
+											</Card.Text>
+											<Card.Link href={businessData.website} target="_blank">website</Card.Link>
+										</Card.Body>
+									</Card>
 								</div>
-								
+
 							</Col>
 							<Col xl={12}>
 								<div className="businessSide">
-								<Card>
-									<Card.Body>
-										<Card.Title>Map</Card.Title>
-										<Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-										<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
-										</Card.Text>
-										<Card.Link href="#">Card Link</Card.Link>
-										<Card.Link href="#">Another Link</Card.Link>
-									</Card.Body>
-								</Card>
+									<Card>
+										<Card.Body>
+											<Card.Title>Map</Card.Title>
+											<Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+											<Card.Text>
+												Some quick example text to build on the card title and make up the bulk of
+												the card's content.
+											</Card.Text>
+											<Card.Link href="#">Card Link</Card.Link>
+											<Card.Link href="#">Another Link</Card.Link>
+										</Card.Body>
+									</Card>
 								</div>
 							</Col>
 							<Col xl={12}>
 								<div className="businessSide">
-								<Card>
-									<Card.Body>
-										<Card.Title>Feedback</Card.Title>
-										<Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-										<Card.Text>
-										Some quick example text to build on the card title and make up the bulk of
-										the card's content.
-										</Card.Text>
-										<Card.Link href="#">Card Link</Card.Link>
-										<Card.Link href="#">Another Link</Card.Link>
-									</Card.Body>
-								</Card>
+									<Card>
+										<Card.Body>
+											<Card.Title>Feedback</Card.Title>
+											<Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+											<Card.Text>
+												Some quick example text to build on the card title and make up the bulk of
+												the card's content.
+											</Card.Text>
+											<Card.Link href="#">Card Link</Card.Link>
+											<Card.Link href="#">Another Link</Card.Link>
+										</Card.Body>
+									</Card>
 								</div>
 							</Col>
 						</Row>
@@ -114,7 +164,7 @@ const Business = (props) => {
 			</Container>
 			<WriteReviewModal
 				show={showReviewModal}
-				onHide={()=>setShowReviewModal(false)}
+				onHide={() => setShowReviewModal(false)}
 				id={id}
 			/>
 		</div>
