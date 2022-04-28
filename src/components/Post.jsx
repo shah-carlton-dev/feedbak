c61a95773687f4202e0fbe96acaab9b19b42cec7
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-04-28 16:48:05 -0400

    im asking for 50k for 10 pecrent

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-import Axios from 'axios'
+import React from 'react';
 import { Card, Button, Row, Col } from 'react-bootstrap';
-import { API_URL } from "../utils/constants";
 
-const Post = ({ postInfo, admin, user, updatePost }) => {
+const Post = ({ postInfo, admin, handleMakeFeatured, sendScoreChange }) => {
 	// console.log(postInfo)
 	let { _id, title, post, score, authorName, featured, date } = postInfo
 	date = new Date(date)
@@ -11,84 +9,27 @@ const Post = ({ postInfo, admin, user, updatePost }) => {
 			{ weekday: "long", year: "numeric", month: "short", day: "numeric", hour: "numeric", minute: "numeric" }
 		)
 
-	const [stateScore, setStateScore] = useState(score)
-	const [stateFeatured, setStateFeatured] = useState(featured)
-
-	const handleMakeFeatured = async () => {
-		const url = `${API_URL}/posts/updateFeatured/${_id}`
-		const info = { busi: postInfo.business }
-		const originalStatus = stateFeatured
-		try {
-			await Axios.put(url, info)
-				.then((res) => {
-					setStateFeatured(!originalStatus)
-				});
-		} catch (err) {
-			console.log("Error while attempting featured change");
-		} finally {
-			updatePost(_id, false, !originalStatus)
-		}
-	}
-
-	const sendAdminScoreChange = async (upvote) => {
-		const url = `${API_URL}/posts/updateScore/admin/${_id}`
-		const info = { upvote }
-		let newScore;
-		try {
-			await Axios.put(url, info)
-				.then((res) => {
-					newScore = res.data.newScore
-					setStateScore(newScore)
-				});
-		} catch (err) {
-			console.log("Error while attempting admin score change");
-		} finally {
-			updatePost(_id, true, newScore)
-		}
-	}
-
-	const sendScoreChangeReq = async (upvote) => {
-		if (admin) {
-			sendAdminScoreChange(upvote);
-			return;
-		}
-		const url = `${API_URL}/posts/updateScore/${_id}`
-		const info = { upvote, user }
-		let newScore;
-		try {
-			await Axios.put(url, info) 
-				.then((res) => {
-					newScore = res.data.newScore
-					setStateScore(newScore)
-				});
-		} catch (err) {
-			console.log("Error while attempting score change");
-		} finally {
-			updatePost(_id, true, newScore)
-		}
-	}
-
 	return (
 		<>
 			<Card className="p-4 post-style">
 				<Row>
 					<Col xs="10">
-						<h2>{title}</h2> 
+						<h2>{title}</h2>
 						<h5>{authorName}</h5>
 						<p>{post}</p>
+						<p>{`score: ${score}`}</p>
 						<p>{featured ? 'featured' : 'not featured'}</p>
 						<p>{date}</p>
-						{admin && <Button onClick={() => handleMakeFeatured()}>Make featured</Button>}
+						{admin && <Button onClick={() => handleMakeFeatured(_id)}>Make featured</Button>}
 						<br />
 					</Col>
 					<Col xs="2" className="my-auto justify-content-center">
-							<Button className="post-button-arrow" onClick={() => sendScoreChangeReq(true)}>⬆️</Button>
-							<span className="post-score">{stateScore}</span>
-							<Button className="post-button-arrow" onClick={() => sendScoreChangeReq(false)}>⬇️</Button>
+						<Button className="post-button-arrow" onClick={() => sendScoreChangeReq(true)}>⬆️</Button>
+						<span className="post-score">{stateScore}</span>
+						<Button className="post-button-arrow" onClick={() => sendScoreChangeReq(false)}>⬇️</Button>
 					</Col>
 				</Row>
 			</Card>
-
 		</>
 	)
 }
